a245e7064a7ea98243a175681afd435bb1a5432b
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-20 12:53:40 +0300

    Добавьте файлы проекта.

---
+++ task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        using (StreamReader streamReader = new StreamReader("text.txt"))
+        {
+            string line = streamReader.ReadToEnd();
+            string[] strings = line.Split('\n');
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    strings[i] = strings[i].Replace('-', '@');
+                    strings[i] = strings[i].Replace(',', '@');
+                    strings[i] = strings[i].Replace('.', '@');
+                    strings[i] = strings[i].Replace('!', '@');
+                    strings[i] = strings[i].Replace('?', '@');
+                    string[] result = strings[i].Split();
+                    for(int j = result.Length - 1; j >= 0; j--)
+                    {
+                        Console.Write(result[j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
