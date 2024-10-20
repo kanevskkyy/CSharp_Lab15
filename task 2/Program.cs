1c0eeb64f62eec8a96ca7b7da7c08995743b15c2
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-20 13:12:18 +0300

    done task 2

---
+++ task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        string path = "text.txt";
+        string[] strings = File.ReadAllLines(path);
+        string[] result = new string[10];
+        for(int i = 0; i < strings.Length; i++)
+        {
+            int lettersAmount = 0, symbolsAmount = 0;
+            for(int j = 0; j < strings[i].Length; j++)
+            {
+                if (char.IsLetter(strings[i][j])) lettersAmount++;
+                else if(strings[i][j] == '-' || strings[i][j] == ',' || strings[i][j] == '`' || strings[i][j] == '.' || strings[i][j] == '?' || strings[i][j] == '!') symbolsAmount++;
+            }
+            result[i] = $"Line {i + 1}: ";
+            result[i] += strings[i] + $"({lettersAmount})({symbolsAmount})";
+        }
+        File.WriteAllLines("result.txt", result);
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
